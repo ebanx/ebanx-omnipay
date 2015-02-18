3cb21da827453427ae42d6aa4b69b0dfec977765
Heitor <heitor@ebanx.com>
2015-02-18 14:45:59 -0200

    general commit

---
+++ src/Gateway.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Omnipay\Ebanx;
+
+use Omnipay\Common\AbstractGateway;
+use Omnipay\Ebanx\Message\Request;
+
+/**
+ * eWAY Rapid 3.0 Gateway
+ */
+class EbanxGateway extends AbstractGateway
+{
+    //public $transparentRedirect = true;
+
+    public function getName()
+    {
+        return 'EBANX Checkout';
+    }
+
+    public function getDefaultParameters()
+    {
+        return array(
+            'integration_key' => '',
+            'testMode' => false,
+        );
+    }
+
+    public function getApiKey()
+    {
+        return $this->getParameter('integration_key');
+    }
+
+    public function setApiKey($value)
+    {
+        return $this->setParameter('integration_key', $value);
+    }
+
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Ebanx\Message\Request', $parameters);
+    }
+
+}
